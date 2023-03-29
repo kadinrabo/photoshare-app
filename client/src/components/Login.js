f3641e810a01b9d292a9bb1e693d6e42888b608b
boon5 <kadinjrabo@gmail.com>
2023-03-29 13:02:02 -0700

    local storage working

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -13,10 +13,28 @@ function Login() {
         fetchUserData();
     }, []);
 
+    const [text, setText] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Submitted text: ${text}`);
+        window.localStorage.setItem('uid', text);
+        const data = JSON.parse(localStorage.getItem('uid'));
+        console.log(`Local storage has user uid: ${data}`);
+  }
+  const handleChange = (event) => {
+    setText(event.target.value);
+  }
+
     return (
         <div>
             <h1>Login page here</h1>
             {user ? (<p>Api active!</p>) : (<p>Api broken?</p>)}
+            <form onSubmit={handleSubmit}>
+                <label> Enter some text: <input type="text" value={text} onChange={handleChange} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
