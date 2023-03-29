ac7ca053b852f2137e6c14d073f99b93612d3baf
boon5 <kadinjrabo@gmail.com>
2023-03-28 22:05:47 -0700

    users/uid path and modular client fetching

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,11 +1,24 @@
 const client = require('../util/database');
 
-// Handler for http://localhost:8080/users. Uses client to do a psql query
+// Handler for http://localhost:8080/users
 exports.getUsers = (req, res, next) => {
   client.query('SELECT * FROM usertable', (err, result) => {
     if (err) {
       return next(err);
     }
     res.json(result.rows);
+  });
+};
+
+// Handler for http://localhost:8080/users/x
+exports.getUser = (req, res, next) => {
+  const userId = req.params.uid;
+  client.query('SELECT * FROM usertable WHERE uid = $1', [userId],
+  (err, result) => {
+    if (err)
+    {
+      return next(err);
+    }
+    res.json(result.rows[0]);
   });
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 
 // Configure a handler for http://localhost:8080/users/
 router.get('/', controller.getUsers); // /users
+router.get('/:uid', controller.getUser); // /users/:uid
 
 module.exports = router;
